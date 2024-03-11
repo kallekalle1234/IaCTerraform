17616d8a5634050a1ae3dfb56b5c8b58e4869af1
kallekalle1234 <felixkristensen@gmail.com>
2024-03-11 08:54:56 +0100

    oppgave modul 7

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.94.0"
+    }
+  }
+  
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-iacmodul6-fk"
+    storage_account_name = "sabeiacmodul6fk001"
+    container_name       = "tfstate"
+    key                  = "rg.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+}
