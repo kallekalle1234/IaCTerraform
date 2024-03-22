143f800b6924fbc6ad034065e8c41cf7a8a5782f
kallekalle1234 <felixkristensen@gmail.com>
2024-03-22 21:44:38 +0100

    modul8

---
+++ modul8/main.tf
@@ -0,0 +1,47 @@
+locals {
+  workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+
+  rg_name        = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspaces_suffix}"
+  sa_name = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}-${local.workspaces_suffix}"
+  source_content = terraform.workspace == "default" ? "<h1>${var.source_content}</h1>" : "<h1>${var.source_content}: ${local.workspaces_suffix}</h1>"
+}
+
+resource "random_string" "random_string" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg_web" {
+  name     = local.rg_name
+  location = var.rg_location
+}
+
+resource "azurerm_storage_account" "sa_web" {
+  name                     = "${lower(local.sa_name)}${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.rg_web.name
+  location                 = azurerm_resource_group.rg_web.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document = var.index_document
+  }
+}
+
+resource "azurerm_storage_blob" "index_html" {
+  name                   = var.index_document
+  storage_account_name   = azurerm_storage_account.sa_web.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = local.source_content
+}
+
+output "rg_name" {
+  value = azurerm_resource_group.rg_web.name
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.sa_web.primary_web_endpoint
+}
